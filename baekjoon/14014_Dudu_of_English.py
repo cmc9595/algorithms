150c3ba0db90c96025ddea6a5d1a2e95389eb7cb
cmc <cmc@cmcui-MacBookAir.local>
2022-12-21 00:37:51 +0900

    221221

--- baekjoon/14014_Dudu_of_English.py
+++ baekjoon/14014_Dudu_of_English.py
@@ -1,31 +1,30 @@
 import re
+import sys
 N=int(input())
-l=[input() for _ in range(N)]
+s=''.join(sys.stdin.readlines())
 of_words = ['of', 'to', 'into', 'onto', 'above', 'below', 'from', 'by', 'is', 'at']
 vowels = ['a','e','i','o','u']
-for s in l:
-    s = s.lower()
-    s = s.split()
-    for i in range(len(s)):
-        if s[i] in of_words:
-            s[i] = 'of'
-    for i in range(len(s)):
-        cnt = sum(s[i].count(v) for v in vowels)
-        cnt //= 2
-        s[i] = ''.join('' if j in vowels and (cnt:=cnt-1)>-1 else j for j in s[i])
-    s = ' '.join(s)
-    s = re.sub('[,.\'"-_\?\!\[\]\{\}\(\)]', '', s)
-    s = re.sub('\n', ' ', s)
-    s = re.sub(' +', ' ', s)
-    tmp = 0
-    for i in s.split():
-        print(i, end='')
-        tmp+=len(i)
-        print(tmp, end='')
-        if tmp>20:
-            print()
-            tmp=0
-        else:
-            print(' ', end='')
+s = s.lower()
+s = s.split()
+for i in range(len(s)):
+    if s[i] in of_words:
+        s[i] = 'of'
+for i in range(len(s)):
+    cnt = sum(s[i].count(v) for v in vowels)
+    cnt //= 2
+    s[i] = ''.join('' if j in vowels and (cnt:=cnt-1)>-1 else j for j in s[i])
+s = ' '.join(s)
+s = re.sub('[,.\'"-_\?\!\[\]\{\}\(\)]', '', s)
+s = re.sub('\n', ' ', s)
+s = re.sub(' +', ' ', s)
+tmp = 0
+for i in s.split():
+    print(i, end='')
+    tmp+=len(i)
+    if tmp>20:
+        print()
+        tmp=0
+    else:
+        print(' ', end='')
             
 
